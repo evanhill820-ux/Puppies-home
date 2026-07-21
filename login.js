ba3b4ded8d82571540387afa671dfdfbb3771f2f
evanhill820-ux <evanhill820@gmail.com>
2026-07-22 00:54:50 +0100

    Update login.js

--- login.js
+++ login.js
@@ -1,1 +1,25 @@
+import { auth } from "./firebase.js";
 
+import {
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+const loginBtn = document.getElementById("loginBtn");
+
+loginBtn.addEventListener("click", async () => {
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+
+    alert("🎉 Login successful!");
+
+    window.location.href = "index.html";
+
+  } catch (error) {
+    alert(error.message);
+  }
+
+});
