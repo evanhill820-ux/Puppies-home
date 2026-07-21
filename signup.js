f8907bc05668f66736d6c24a934582d691190f43
evanhill820-ux <evanhill820@gmail.com>
2026-07-22 00:51:04 +0100

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,1 +1,25 @@
+import { auth } from "./firebase.js";
 
+import {
+  createUserWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+const signupBtn = document.getElementById("signupBtn");
+
+signupBtn.addEventListener("click", async () => {
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  try {
+    await createUserWithEmailAndPassword(auth, email, password);
+
+    alert("🎉 Account created successfully!");
+
+    window.location.href = "login.html";
+
+  } catch (error) {
+    alert(error.message);
+  }
+
+});
